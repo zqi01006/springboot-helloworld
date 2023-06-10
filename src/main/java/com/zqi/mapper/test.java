cab93df64ecf89ca9572b1594ee7e92a9a104a4f
zqi <1317080367@qq.com>
2023-06-10 14:53:34 +0800

    test类likelihood属性

--- src/main/java/com/zqi/mapper/test.java
+++ src/main/java/com/zqi/mapper/test.java
@@ -3,8 +3,6 @@ package com.zqi.mapper;
 public class test {
     String likelihood;
     String likelihood1;
-    String likelihood2;
-    String likelihood3;
     String likelihood4;
     String likelihood5;
 }
