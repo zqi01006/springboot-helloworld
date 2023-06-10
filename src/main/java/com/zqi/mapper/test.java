47d99612defdf4a507df70182dc304cc3cac3df0
wuwenshuo <wuwenshuo@maxtropy.com>
2023-06-10 14:52:52 +0800

    feat: 添加输出

--- src/main/java/com/zqi/mapper/test.java
+++ src/main/java/com/zqi/mapper/test.java
@@ -2,4 +2,9 @@ package com.zqi.mapper;
 
 public class test {
     String likelihood;
+    String likelihood1;
+    String likelihood2;
+    String likelihood3;
+    String likelihood4;
+    String likelihood5;
 }
